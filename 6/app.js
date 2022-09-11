4bddb65d6d5e1a921ff081604685eaccb32209e1
lucy <kkadangj@gmail.com>
2022-09-11 20:34:37 +0900

    # morgan, express-session, cookie-parser

--- 6/app.js
+++ 6/app.js
@@ -1,8 +1,32 @@
 const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
 const path = require("path");
+
+dotenv.config()
 const app = express()
 
 app.set('port', process.env.PORT || 3000); // 속성 추가  전역변수
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COKKIE_SECRET));
+console.log(process.env.COOKIE_SECRET);
+
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false
+    },
+    name: 'session-cookie'
+}))
+
 app.use((req, res, next) => {
     console.log('모든 요청에 실행하고 싶어요')
     next()
